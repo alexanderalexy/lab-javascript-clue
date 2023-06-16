909c17205209cd7f71db4e69d0f0d27ca1b0b975
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-06-16 08:13:07 +0200

    Solved lab

--- src/clue.js
+++ src/clue.js
@@ -3,14 +3,177 @@
 // Suspects Array
 
 const suspectsArray = [];
+const susp1 = {
+    
+    firstName: `Jacob`,
+    lastName: `Green`,
+    occupation: `Entrepreneur`,
+    age: 45,
+    description: `He has a lot of connections`,
+    image: `https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg`,
+    color: `green`
+}
+const susp2 = {
+    firstName: `Doctor`,
+    lastName: `Orchid`,
+    occupation: `Scientist`,
+    age: 26,
+    description: `PhD in plant toxicology. Adopted daughter of Mr. Boddy`,
+    image: `http://www.radiotimes.com/uploads/images/Original/111967.jpg`,
+    color: `white`
+}
+
+const susp3 = {
+    firstName: `Victor`,
+    lastName: `Plum`,
+    occupation: `Designer`,
+    age: 22,
+    description: `Billionaire video game designer`,
+    image: `https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg`,
+    color: `purple`
+}
+
+const susp4 = {
+    
+    firstName: `Kasandra`,
+    lastName: `Scarlet`,
+    occupation: `Actor`,
+    age: 31,
+    description: `She is an A-list movie star with a dark past`,
+    image: `https://www.radiotimes.com/uploads/images/Original/111967.jpg`,
+    color: `red`
+
+}
+
+const susp5 = {
+    firstName: `Eleanor`,
+    lastName: `Peacock`,
+    occupation: `Socialité`,
+    age: 36,
+    description: `She is from a wealthy family and uses her status and money to earn popularity`,
+    image: `https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg`,
+    color: `blue`,
+}
+
+const susp6 = {
+    firstName: `Jack`,
+    lastName: `Mustard`,
+    occupation: `Retired Football player`,
+    age: 62,
+    description: `He is a former football player who tries to get by on his former glory`,
+    image: `https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg`,
+    color: `yellow`,
+
+}
+
+suspectsArray.unshift(susp1);
+suspectsArray.push(susp2);
+suspectsArray.push(susp3);
+suspectsArray.push(susp4);
+suspectsArray.push(susp5);
+suspectsArray.push(susp6);
+
+
 
 // Rooms Array
 
 const roomsArray = [];
 
+const room1 = {name: `Dining Room`}
+const room2 = {name: `Conservatory`}
+const room3 = {name: ` Kitchen`}
+const room4 = {name: `Study`}
+const room5 = {name: `Library`}
+const room6 = {name: `Billiard Room`}
+const room7 = {name: `Lounge`}
+const room8 = {name: `Ballroom`}
+const room9 = {name: `Hall`}
+const room10 = {name: `Spa`}
+const room11= {name: `Living Room`}
+const room12 = {name: `Observatory`}
+const room13 = {name: `Theater`}
+const room14 = {name: `Guest House`}
+const room15 = {name: `Patio`}
+
+roomsArray.unshift(room1);
+roomsArray.push(room2);
+roomsArray.push(room3);
+roomsArray.push(room4);
+roomsArray.push(room5);
+roomsArray.push(room6);
+roomsArray.push(room7);
+roomsArray.push(room8);
+roomsArray.push(room9);
+roomsArray.push(room10);
+roomsArray.push(room11);
+roomsArray.push(room12);
+roomsArray.push(room13);
+roomsArray.push(room14);
+roomsArray.push(room15);
+
+
 // Weapons Array
 
 const weaponsArray = [];
+
+const wep1 = {
+    name: `rope`,
+    weight: 10
+}
+
+const wep2 = {
+    name: `knife`,
+    weight: 8
+}
+
+const wep3 = {
+    name: `candlestick`,
+    weight: 2
+}
+
+const wep4 = {
+    name: `dumbbell`,
+    weight: 30
+} 
+
+const wep5 = {
+    name: `poison`,
+    weight: 2
+}
+
+const wep6 = {
+    name: `axe`,
+    weight: 15
+}
+
+const wep7 = {
+    name: `bat`,
+    weight: 13
+}
+
+const wep8 = {
+    name: `trophy`,
+    weight: 25
+}
+
+const wep9 = {
+    name: `pistol`,
+    weight: 20
+}
+
+weaponsArray.unshift(wep1);
+weaponsArray.push(wep2);
+weaponsArray.push(wep3);
+weaponsArray.push(wep4);
+weaponsArray.push(wep5);
+weaponsArray.push(wep6);
+weaponsArray.push(wep7);
+weaponsArray.push(wep8);
+weaponsArray.push(wep9);
+
+
+
+
 
 
 // ITERATION 2
